ea5879bda600806591d9359b6283de33f2e2c392
Nageshn10 <nandigamnagesh29500@gmail.com>
2026-02-15 01:56:36 +0530

    Update erp_loc_a101_validation.sql

--- scripts/validation/erp_loc_a101_validation.sql
+++ scripts/validation/erp_loc_a101_validation.sql
@@ -1,1 +1,23 @@
+/*
+========================================================
+TABLE : erp_loc_a101
+LAYER : Bronze → Silver
+PURPOSE: Validate and standardize location information
+RULE  : All queries should return ZERO rows
+========================================================
+*/
+
+-- Null or empty customer IDs
+SELECT *
+FROM bronze.erp_loc_a101
+WHERE CID IS NULL OR CID = '';
+
+-- Country values review (standardization check)
+SELECT DISTINCT CNTRY
+FROM bronze.erp_loc_a101;
+
+-- Unwanted spaces
+SELECT *
+FROM bronze.erp_loc_a101
+WHERE CNTRY <> TRIM(CNTRY);
 
