61f345530f6abdf490e495d94a84d4b7cffe761e
Nageshn10 <nandigamnagesh29500@gmail.com>
2026-02-17 04:24:55 +0530

    Update erp_px_cat_g1v2_validation.sql

--- scripts/validation/erp_px_cat_g1v2_validation.sql
+++ scripts/validation/erp_px_cat_g1v2_validation.sql
@@ -1,28 +1,27 @@
 /*
 ========================================================
-TABLE   : erp_px_cat_g1v2
+TABLE   : silver.erp_px_cat_g1v2
 LAYER   : Silver (Clean Layer)
 PURPOSE : Validate product category and subcategory data
 RULE    : All queries must return ZERO rows
 ========================================================
 */
 
--- Check 1: Category and subcategory must not be null or blank
+-- Category and subcategory must not be null or blank
 SELECT *
-FROM bronze.erp_px_cat_g1v2
+FROM silver.erp_px_cat_g1v2
 WHERE CAT IS NULL OR CAT = ''
    OR SUBCAT IS NULL OR SUBCAT = '';
 
-
--- Check 2: Remove unwanted spaces
+-- No unwanted spaces
 SELECT *
-FROM bronze.erp_px_cat_g1v2
+FROM silver.erp_px_cat_g1v2
 WHERE CAT <> TRIM(CAT)
    OR SUBCAT <> TRIM(SUBCAT);
 
-
--- Check 3: Distinct category review
+-- Review standardized category values
 SELECT DISTINCT CAT, SUBCAT
-FROM bronze.erp_px_cat_g1v2;
+FROM silver.erp_px_cat_g1v2;
+
 
 
