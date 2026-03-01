d7ed2213acec7ae4204916200f67e0d5590f0638
Nageshn10 <nandigamnagesh29500@gmail.com>
2026-03-01 23:48:29 +0530

    Update crm_sales_details_validation.sql

--- scripts/validation/crm_sales_details_validation.sql
+++ scripts/validation/crm_sales_details_validation.sql
@@ -7,13 +7,15 @@ RULE    : All queries must return ZERO rows
 ========================================================
 */
 
--- Check 1: Order date must exist
-SELECT *
-FROM silver.crm_sales_details
-WHERE sls_order_dt IS NULL;
+-- Check 1: check for invalid dates
+SELECT nullif(sls_cust_id,0) FROM bronze.crm_sales_details
+where sls_order_dt <=0
+or length(sls_order_dt) !=8 
+or sls_order_dt >20500101
+or sls_ship_dt <19000101
 
 
--- Check 2: Date sequence must be correct
+-- Check 2: Invalid Date Orders
 -- order <= ship <= due
 SELECT *
 FROM silver.crm_sales_details
@@ -38,11 +40,19 @@ SELECT *
 FROM silver.crm_sales_details
 WHERE sls_sales <> sls_quantity * sls_price;
 
+-- Check 5:
+--  data consistency : Between Sales,Quantity,and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative.
 
--- Check 6: No missing critical fields
-SELECT *
-FROM silver.crm_sales_details
-WHERE sls_prd_key IS NULL
-   OR sls_cust_id IS NULL;
+SELECT distinct
+sls_sales,
+sls_quantity,
+sls_price 
+from bronze.crm_sales_details
+where sls_sales!= sls_quantity *sls_price
+or sls_sales is null or sls_quantity is null or sls_price is null
+or sls_sales <=0 or sls_quantity <=0 or sls_price <=0
+order by sls_sales,sls_quantity,sls_price
 
 
